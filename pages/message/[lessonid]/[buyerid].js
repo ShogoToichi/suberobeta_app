06de4324adc61d7c51da515ef2857f76c207cbac
TakumiOsato <takumi.0530.tessin@icolud.com>
2021-03-06 00:42:55 +0900

    messageの微調整

--- pages/message/[lessonid]/[buyerid].js
+++ pages/message/[lessonid]/[buyerid].js
@@ -1,10 +1,14 @@
 import React from "react"
 import Message from "../../../components/message/Message"
 import Layout from "../../../components/common/Layout"
+import Container from "@material-ui/core/Container"
+
 
 const message = () => (
   <Layout>
+    <Container maxWidth="lg">
     <Message />
+    </Container>
   </Layout>
 )
 
